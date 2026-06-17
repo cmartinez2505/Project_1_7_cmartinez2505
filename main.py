fd948e2980b6ece410381877010846b13ea53108
Chris <chrismtz2003@gmail.com>
2026-06-17 17:02:33 -0400

    I corrected the spelling in my header

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
 Program Name: Insurance Quote Generator App
 Author: Chris Martinez
-Program Purpose: A user frinedly tool that will ask customers basic questions like their age and driving history,
-and then it will calculate their car insurance price depending on their answer, adn then it will save that data into a profile.
+Program Purpose: A user friendly tool that will ask customers basic questions like their age and driving history,
+and then it will calculate their car insurance price depending on their answer, and then it will save that data into a profile.
 Resources Used: Python Crash Course (Chapters 1-7)
 Date: June 17 2026
 """
