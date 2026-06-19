a27e09b06f1af41d0dafc03ada241f5b0bd97f8b
Chris <chrismtz2003@gmail.com>
2026-06-19 01:17:02 -0400

    Moved my If statements into my backend calculate_quote function

--- main.py
+++ main.py
@@ -7,6 +7,49 @@ Resources Used: Python Crash Course (Chapters 1-7)
 Date: June 17 2026
 """
 
+def calculate_quote(age, car_year, salary, driving_years, accidents):
+    """This function calculates the final monthly insurance cost based on customer information and driving history"""
+    base_insurance = 100.00
+    final_insurance = base_insurance
+
+    #If Statement #1 (A surcharge of $25 for young drivers that are 25 and under.)
+    if age <= 25:
+        final_insurance += 25.00
+    else:
+        pass;   
+
+    #If Statement #2 (A discount of $15 for cars older then 15 years old and a $15 addition for cars newer 2023.)
+    if car_year <= 2011:
+        final_insurance -= 15.00
+    elif car_year >= 2024:
+        final_insurance += 15.00
+    else:
+        pass
+
+    #If Statement #3 (A 10 percent surcharge is given for those who make over $200,000 annualy and a 10 discount for those who make $40,000 or less a year.)
+    if salary >= 200000:
+     final_insurance += final_insurance * 0.10
+    elif salary <= 40000:
+     final_insurance -= final_insurance * 0.10    
+    else:
+     pass
+
+    #If Statement #4 (A extra 10 percent discount is given to drivers who drove a car for atleast 5 years or more.)
+    if driving_years >= 5:
+     final_insurance -= final_insurance * 0.10
+    else:
+      pass
+
+    #If Statement #5 (A extra 5 percent surcharge is given for those who had gotten into 2 or more car accidents.)
+    if accidents >= 2:
+     final_insurance += final_insurance * 0.05
+    else:
+      pass
+
+    return final_insurance
+
+
+
 print("********************************")
 print("   Welcome to MTZ Insurance!   ")
 print("********************************")
@@ -20,39 +63,4 @@ salary = float(input("Please enter your annual salary in digits: "))
 driving_years = int(input("How many years have you been driving? ")) 
 accidents = int(input("How many vehicle accidents have you gotten in? ")) 
 
-base_insurance = 100.00
-final_insurance = base_insurance
 
-#If Statement #1 (A surcharge of $25 for young drivers that are 25 and under.)
-if age <= 25:
-    final_insurance += 25.00
-else:
-    pass;   
-
-#If Statement #2 (A discount of $15 for cars older then 15 years old and a $15 addition for cars newer 2023.)
-if car_year <= 2011:
-    final_insurance -= 15.00
-elif car_year >= 2024:
-    final_insurance += 15.00
-else:
-    pass
-
-#If Statement #3 (A 10 percent surcharge is given for those who make over $200,000 annualy and a 10 discount for those who make $40,000 or less a year.)
-if salary >= 200000:
-    final_insurance += final_insurance * 0.10
-elif salary <= 40000:
-    final_insurance -= final_insurance * 0.10    
-else:
-    pass
-
-#If Statement #4 (A extra 10 percent discount is given to drivers who drove a car for atleast 5 years or more.)
-if driving_years >= 5:
-    final_insurance -= final_insurance * 0.10
-else:
-    pass
-
-#If Statement #5 (A extra 5 percent surcharge is given for those who had gotten into 2 or more car accidents.)
-if accidents >= 2:
-    final_insurance += final_insurance * 0.05
-else:
-    pass    
