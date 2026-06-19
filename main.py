3538e2c3f9cde04078e191f3a67d070463e543ec
Chris <chrismtz2003@gmail.com>
2026-06-19 01:44:04 -0400

    Added () to the end of the lower function in line 63

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ while running == True:
 
     customer_name = input("Please enter your full name (or type quit to exit program): ").strip()
 
-    if customer_name.lower == 'quit':
+    if customer_name.lower() == 'quit':
        running = False
        continue
 
